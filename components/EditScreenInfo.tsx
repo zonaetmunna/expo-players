e54f00d3248483e5e0d32462eae55a0f6cd6d73f
zonaetmunna <zonaetmonna@gmail.com>
2026-04-28 15:44:00 +0600

    initlized

---
+++ components/EditScreenInfo.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, View } from 'react-native';
+
+import { Text } from '@/components/nativewindui/Text';
+
+
+
+export default function EditScreenInfo({ path }: { path: string }) {
+
+  const title = "Open up the code for this screen:"
+  const description = "Change any of the text, save the file, and your app will automatically update."
+
+  return (
+    <View style={styles.getStartedContainer}>
+      <Text variant="heading" className="text-center">
+        {title}
+      </Text>
+      <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+        <Text>{path}</Text>
+      </View>
+      <Text variant="body" className="text-center">
+        {description}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+});
---
+++ components/ScreenContent.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Text } from '@/components/nativewindui/Text';
+
+import EditScreenInfo from './EditScreenInfo';
+
+type ScreenContentProps = {
+  title: string;
+  path: string;
+  children?: React.ReactNode;
+};
+
+export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
+  return (
+    <View style={styles.container}>
+      <Text variant="title1" className="text-center">
+        {title}
+      </Text>
+      <View style={styles.separator} />
+      <EditScreenInfo path={path} />
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  separator: {
+    backgroundColor: '#d1d5db',
+    height: 1,
+    marginVertical: 30,
+    width: '80%',
+  },
+});
